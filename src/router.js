d774465d9924dbdaa067960b11147346b80f52e1
Irene <ruizdacal@gmail.com>
2022-08-28 15:13:36 +0200

    added basic structure

---
+++ src/router.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from './views/HomeView.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'root',
+    component: HomeView,
+  },
+//   {
+//     path: '/list',
+//     name: 'list',
+//     component: () => import(/* webpackChunkName: 'list' */ './pages/CountriesList.vue'),
+//     children: [
+//       {
+//         path: '/details',
+//         name: 'details',
+//         component: () => import(/* webpackChunkName: 'details' */ './pages/CountriesDetails.vue')
+//       },
+//     ]
+//   }
+];
+
+export default createRouter({
+  history: createWebHistory('/'),
+  routes,
+  scrollBehavior() {
+    document.getElementById('app').scrollIntoView();
+  }
+});
